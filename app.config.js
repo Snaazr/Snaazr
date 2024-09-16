4ae8c8c2e98eda16dac376d748b609afade2d214
Supriyo <supriyomalstartup@gmail.com>
2024-09-17 00:17:53 +0530

    #20 feat: Implement and style Notification component
    - Added a notification screen with a list of notifications.
    - Included functionality to mark notifications as read and clear all notifications.
    - Implemented a custom badge to display unread notification count.
    - Improved UI with SafeAreaView to prevent overlapping with device notches.
    - Enhanced styling for notification items and header for a modern look.
    - Added handling for empty state when no notifications are present.

--- app.config.js
+++ app.config.js
@@ -1,32 +1,36 @@
 module.exports = {
-    expo: {
-      name: "Snaazr",
-      slug: "Snaazr",
-      version: "1.0.0",
-      orientation: "portrait",
-      icon: "./assets/snaazr.png",
-      userInterfaceStyle: "light",
-      splash: {
-        image: "./assets/splash.png",
-        resizeMode: "contain",
+  expo: {
+    name: "Snaazr",
+    slug: "Snaazr",
+    version: "1.0.1",
+    orientation: "portrait",
+    icon: "./assets/snaazr.png",
+    userInterfaceStyle: "light",
+    splash: {
+      image: "./assets/splash.png",
+      resizeMode: "contain",
+      backgroundColor: "#ffffff",
+    },
+    assetBundlePatterns: ["**/*"],
+    ios: {
+      supportsTablet: true,
+      bundleIdentifier: "com.snaazr.app",
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: "./assets/adaptive-icon.png",
         backgroundColor: "#ffffff",
       },
-      assetBundlePatterns: ["**/*"],
-      ios: {
-        supportsTablet: true,
-        bundleIdentifier: "com.snaazr.app",
-      },
-      android: {
-        adaptiveIcon: {
-          foregroundImage: "./assets/adaptive-icon.png",
-          backgroundColor: "#ffffff",
-        },
-        package: "com.snaazr.app",
-      },
-      web: {
-        favicon: "./assets/favicon.png",
-      },
-      plugins: ["expo-font"],
+      package: "com.snaazr.app",
     },
-  };
-  
+    web: {
+      favicon: "./assets/favicon.png",
+    },
+    plugins: ["expo-font"],
+    extra: {
+      eas: {
+        projectId: "39d2df18-1940-42da-bbdb-942073d6e1a1",
+      },
+    },
+  },
+};
